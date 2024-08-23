c58167f0527c8e26af68564dd1dc021363698edb
onise2001 <onise.babulashvili@yahoo.com>
2024-08-23 14:30:02 +0400

    Changed global style settings

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,5 @@
-declare module "./styled-components/GlobalStyles.js" {
-  const GlobalStyles: any;
+declare module "../styled-components/GlobalStyles" {
+  import { createGlobalStyle } from "styled-components";
+  const GlobalStyles: ReturnType<typeof createGlobalStyle>;
   export default GlobalStyles;
 }
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import Header from "../components/Header";
 import { Outlet, useParams, useNavigate } from "react-router-dom";
-import GlobalStyles from "../styled-components/GlobalStyles.js";
+import GlobalStyles from "../my-styled-components/GlobalStyles";
+
 import planets from "../data.json";
 
 export default function Layout() {
